dbe154f829f0c923138ef19b4b9cd5c1425bc936
neslykara <134239433+neslykara@users.noreply.github.com>
2024-09-22 15:58:14 +0300

    v_düzeltme

--- src/main/java/tekrar/StringManipulation.java
+++ src/main/java/tekrar/StringManipulation.java
@@ -13,37 +13,7 @@ public class StringManipulation {
         v)En az bir tane rakam olsun
         vi) En az 1 sembol icermeli*/
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Lutfen parolanızı giriniz");
-        String pwd = input.nextLine();
-
-        boolean lengthControl = pwd.length() > 7;
-        //boolean spaceControl = pwd.replaceAll("[^ ]", "").length() == 0;
-        boolean spaceControl = !pwd.contains(" ");
-        boolean upperCaseControl = pwd.replaceAll("[^A-Z]", "").length() > 0;
-        boolean lowerCaseControl = pwd.replaceAll("[^a-z]" , "").length()>0;
-        boolean digitControl = pwd.replaceAll("[^0-9]", "").length()>0;
-        boolean sembolControl = pwd.replaceAll("[^\\p{Punct}]" , "").length()>0;
-
-        /*System.out.println("lengthControl = " + lengthControl);
-        System.out.println("spaceControl = " + spaceControl);
-        System.out.println("upperCaseControl = " + upperCaseControl);
-        System.out.println("lowerCaseControl = " + lowerCaseControl);
-        System.out.println("digitControl = " + digitControl);
-        System.out.println("sembolControl = " + sembolControl);*/
-
-        boolean isValid = lengthControl && spaceControl && upperCaseControl && lowerCaseControl && digitControl && sembolControl;
-
-        if (isValid)
-        {
-            System.out.println("Sifre gecerlidir");
-        }
-        else
-        {
-            System.out.println("Sifre gecersizdir");
-        }
-
-
+       
 
 
     }
